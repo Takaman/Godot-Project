bbdc144e7fb33a991080456a194c93c029b7caa6
Clarence <2102420@sit.singaporetech.edu.sg>
2023-06-05 07:46:02 +0800

    Fix leftside movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+#Constant values for movement and physics
 const SPEED = 5000
 const MAX_SPEED = 100
 const FRICTION = 350
 const JUMP_VELOCITY = -400.0
+
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity");
 #Needs the ready function to instantiate the animationPlayer. Need to get access to the node of your child
@@ -17,14 +19,21 @@ func _physics_process(delta):
 	velocity = velocity.normalized()
 	
 	
-	#For more natural movement to hit max speed and bringing it down for natural movement\
+	#Flipping of animation to run left as well. Sprite sheet limited
 	if velocity != Vector2.ZERO:
-		animationPlayer.play("runright")
+		if velocity.x > 0:
+			animationPlayer.play("runright")
+			$AnimatedSprite2D.flip_h = false
+		else:
+			animationPlayer.play("runright")
+			$AnimatedSprite2D.flip_h = true
+		
+		#For more natural movement to hit max speed and bringing it down for natural movement
 		velocity = velocity.move_toward(velocity * MAX_SPEED, SPEED * delta)
 	else:
 		animationPlayer.play("idleright")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	
-	
+	#Moving and sliding the character
 	move_and_slide()
