2b0605be335e308d304525831a517799afcaf07d
leechernhao <2100723@sit.singaporetech.edu.sg>
2023-06-14 11:38:20 +0800

    Dialogue make it dynamic
    I made the dialogue more dynamic

--- Enemy/cyclop_boss.gd
+++ Enemy/cyclop_boss.gd
@@ -1,63 +1,41 @@
 extends StaticBody2D
 
-# Constants for movement and physics
 const SPEED = 100
 const MAX_HEIGHT = 100
 const MIN_HEIGHT = 0
 
-# Variables for movement and physics
 var direction = 1
-var dialog_shown = false #Tracking whether dialog has already been shown
-var dialog_open_time = 0 #Tracking how long the dialog has already been open
-var close_dialog = false #Whether dialog can be closed or not
+var dialog_shown = false
+var dialog_open_time = 0
+var close_dialog = false
 @onready var animationPlayer = get_node("AnimatedSprite2D")
-@onready var cyclop_dialog_box = get_node("/root/Level1/CanvasLayer2/cyclop_dialog")
-@onready var cyclop_dialog_label = get_node("/root/Level1/CanvasLayer2/cyclop_dialog/TextureRect/Label2")
+
+var dialogue_start: String = "start"
+var dialogue_resource: DialogueResource
 
 func _ready():
 	add_to_group("Boss")
-	#Connecting with player signal
 	var player = get_node("/root/Level1/Player")
-	player.connect("collided_with_boss", Callable(self , "_on_player_collided_with_boss"))
+	player.connect("collided_with_boss", Callable(self, "_on_player_collided_with_boss"))
 	animationPlayer.play("idle")
-	process_mode = Node.PROCESS_MODE_ALWAYS #So that the node still processes after pausing for dialog
-	
-# Called when the node enters the scene tree for the first time.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#If the dialog has been set to true, it runs the timer
-	if dialog_shown:
-		dialog_open_time += delta
-		if dialog_open_time > 2:
-			close_dialog = true
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
+	# Load the dialogue resource from the file
+	dialogue_resource = load("res://dialogue/main.dialogue")
 
-		
-	
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
-		
-		#Check if the dialog is still typing
-		if cyclop_dialog_label.typing: 
-				cyclop_dialog_label.text = cyclop_dialog_label.full_text
-				cyclop_dialog_label.typing = false
-				cyclop_dialog_label.timer.stop()
-		
-		#A key is pressed to close the dialog
-		elif close_dialog:
-			cyclop_dialog_box.hide()
-			get_tree().paused = false
-			dialog_open_time = 0
-			close_dialog = false
+		if close_dialog:
+			hide_dialogue() # Close the dialogue
 
 func show_popout_interface():
-	cyclop_dialog_label.start_typing()
-	cyclop_dialog_box.show()
-	get_tree().paused = true
-	
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start) # Show the dialogue using the DialogueManager plugin
+
+func hide_dialogue():
+	DialogueManager.hide_dialogue() # Hide the dialogue using the DialogueManager plugin
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player") and not dialog_shown:
 		print("Detected!")
 		show_popout_interface()
 		dialog_shown = true
-	
