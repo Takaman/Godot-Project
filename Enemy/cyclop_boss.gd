455c27a3b7e737975d9a44d75d3c9da9cf1087b4
cyrus <cyrusthedark@gmail.com>
2023-06-16 17:32:55 +0800

    Fixed player moving while dialogue box is open
    Added actionable collision spaces

--- Enemy/cyclop_boss.gd
+++ Enemy/cyclop_boss.gd
@@ -15,27 +15,27 @@ var dialogue_resource: DialogueResource
 
 func _ready():
 	add_to_group("Boss")
-	var player = get_node("/root/Level1/Player")
-	player.connect("collided_with_boss", Callable(self, "_on_player_collided_with_boss"))
+	#var player = get_node("/root/Level1/Player")
+	#player.connect("collided_with_boss", Callable(self, "_on_player_collided_with_boss"))
 	animationPlayer.play("idle")
-	process_mode = Node.PROCESS_MODE_ALWAYS
+	#process_mode = Node.PROCESS_MODE_ALWAYS
 
 	# Load the dialogue resource from the file
-	dialogue_resource = load("res://dialogue/main.dialogue")
+	#dialogue_resource = load("res://dialogue/main.dialogue")
 
-func _input(event):
-	if event is InputEventKey and event.is_pressed():
-		if close_dialog:
-			hide_dialogue() # Close the dialogue
+#func _input(event):
+#	if event is InputEventKey and event.is_pressed():
+#		if close_dialog:
+#			hide_dialogue() # Close the dialogue
 
-func show_popout_interface():
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start) # Show the dialogue using the DialogueManager plugin
+#func show_popout_interface():
+#	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start) # Show the dialogue using the DialogueManager plugin
 
-func hide_dialogue():
-	DialogueManager.hide_dialogue() # Hide the dialogue using the DialogueManager plugin
+#func hide_dialogue():
+#	DialogueManager.hide_dialogue() # Hide the dialogue using the DialogueManager plugin
 
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("Player") and not dialog_shown:
-		print("Detected!")
-		show_popout_interface()
-		dialog_shown = true
+#func _on_area_2d_body_entered(body):
+#	if body.is_in_group("Player") and not dialog_shown:
+#		print("Detected!")
+#		show_popout_interface()
+#		dialog_shown = true
--- Player/Player.gd
+++ Player/Player.gd
@@ -13,41 +13,50 @@ signal healthChanged
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity");
 #Needs the ready function to instantiate the animationPlayer. Need to get access to the node of your child
 @onready var animationPlayer = get_node("AnimatedSprite2D")
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+var dialoguecheck: bool = false
+var input_vector: Vector2 = Vector2.ZERO
 
 #Add the player to a group
 func _ready():
 	add_to_group("Player") 
 
-#Signal for collision detection
-signal collided_with_boss
-
 func _trigger_decreasehealth():
 	currentHealth -= 1
 	healthChanged.emit(currentHealth)
 
-func _physics_process(delta):
-	#Getting the input direction and handle movement and acceleration
-	#Normalising means speed will be the same with two keys pressed when compared to one key
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	velocity = velocity.normalized()
-	
-	
-	#Flipping of animation to run left as well. Sprite sheet limited
-	if velocity != Vector2.ZERO:
-		if velocity.x > 0:
-			animationPlayer.play("runright")
-			$AnimatedSprite2D.flip_h = false
-		else:
-			animationPlayer.play("runright")
-			$AnimatedSprite2D.flip_h = true
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+			var actionables = actionable_finder.get_overlapping_areas()
+			if actionables.size() > 0:
+				dialoguecheck = true
+				actionables[0].action()
+				return
+	dialoguecheck = false
+
+func _physics_process(delta) -> void:
+	if dialoguecheck == false:
+		#Getting the input direction and handle movement and acceleration
+		#Normalising means speed will be the same with two keys pressed when compared to one key
+		velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		velocity = velocity.normalized()
+		#Flipping of animation to run left as well. Sprite sheet limited
+		if velocity != Vector2.ZERO:
+			if velocity.x > 0:
+				animationPlayer.play("runright")
+				$AnimatedSprite2D.flip_h = false
+			else:
+				animationPlayer.play("runright")
+				$AnimatedSprite2D.flip_h = true
+			
+			#For more natural movement to hit max speed and bringing it down for natural movement
+			velocity = velocity.move_toward(velocity * MAX_SPEED, SPEED * delta)
+		else:		
+			animationPlayer.play("idleright")
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-		#For more natural movement to hit max speed and bringing it down for natural movement
-		velocity = velocity.move_toward(velocity * MAX_SPEED, SPEED * delta)
-	else:		
-		animationPlayer.play("idleright")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	
-	#Moving and sliding the character
-	move_and_slide()
+		
+		#Moving and sliding the character
+		move_and_slide()
