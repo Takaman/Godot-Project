0314b45c6c8704937716f0cc62211cccae651551
cyrus <cyrusthedark@gmail.com>
2023-06-23 10:06:55 +0800

    fixed crash

--- World/phishingemail/Sam.gd
+++ World/phishingemail/Sam.gd
@@ -3,7 +3,8 @@ extends Sprite2D
 @onready var hud := $"/root/house_internal_phishingemail/HUD"
 
 func interact() -> void:
-	get_node("marker").queue_free()
+	if get_node("marker") is Node:
+		get_node("marker").queue_free()
 	print("interaction started")
 	hud.show_dialog(
 		"Sam",
