4f919faae5d6e72ca76684e0c336c19e0e3880ca
cyrus <cyrusthedark@gmail.com>
2023-06-27 14:01:49 +0800

    removed unnecessary line

--- World/office1/Laptop3 - question.gd
+++ World/office1/Laptop3 - question.gd
@@ -83,11 +83,6 @@ func _on_area_2d_area_entered(area):
 	if area.is_in_group("Player"):
 		interact()
 
-
-func _physics_process(delta: float) -> void:
-	interactable.visible  = !Score.has_interacted("laptop3","policy")
-
-
 func _on_hud_correct():
 	state = 1
 
