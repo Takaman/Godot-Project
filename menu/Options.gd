3760097e2204cd6ca790d682af0b08695fcc5020
Yap Jia Hao <y_jiahao@yahoo.com>
2023-06-28 21:25:50 +0800

    Global background music

--- menu/Options.gd
+++ menu/Options.gd
@@ -17,33 +17,11 @@ func Fullscreen_on_check_box_toggled(button_pressed):
 	else:
 		DisplayServer.window_set_mode(0) # Set default mode
 
-
 func borderless_on_check_box_2_toggled(button_pressed):
 	if button_pressed:
 		DisplayServer.window_set_mode(2) # 
 	else:
 		DisplayServer.window_set_mode(0) # Set default mode	 
 
-
-func master_on_h_slider_value_changed(value):
-	volume(0,value)
-	
-func volume(bus_index, value):
-	if value == 0:
-		AudioServer.set_bus_volume_db(bus_index, -80)
-	else:
-		var LinearVolume = value/70.0
-		var dbVolume = LinearVolume * 80 - 80
-		AudioServer.set_bus_volume_db(bus_index, dbVolume)
-
-func mute_on_check_box_toggled(button_pressed):
-	if button_pressed:
-		AudioServer.set_bus_volume_db(0, -80)
-	else:
-		AudioServer.set_bus_volume_db(0, 0)
-
-
-	
-		
-
-		
+func _on_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
