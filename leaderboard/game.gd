196eacf4e1f3158c942c4a8de7c79edb82e9652f
leechernhao <2100723@sit.singaporetech.edu.sg>
2023-06-30 04:31:24 +0800

    Leaderboard(hardcoded)-score is stored in leaderboard.dat file

---
+++ leaderboard/game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+var time = 10
+##
+
+
+func _on_Timer_timeout():
+	time -= 1
+	$TimeLabel.text = "TIME: "+str(time)
+	if time <=0:
+		get_tree().change_scene_to_file("res://leaderboard/gameover.tscn")
+
+func _unhandled_input(event):
+	if event is InputEventKey and not event.echo:
+		Globals.score += 1
+		$ScoreLabel.text = "SCORE: "+str(Globals.score)
+		$icon.position.y = Globals.score * 5
