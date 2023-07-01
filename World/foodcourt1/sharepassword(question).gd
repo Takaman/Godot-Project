39ede9377a26e20a5ff6ed8a955f45809a625515
leechernhao <2100723@sit.singaporetech.edu.sg>
2023-07-02 02:54:47 +0800

    added new dialogue for sharepassword

---
+++ World/foodcourt1/sharepassword(question).gd
@@ -0,0 +1,44 @@
+extends Sprite2D
+
+
+@onready var hud := $"/root/FoodCourt/HUD"
+func interact() -> void:
+	print("interaction started")
+	hud.show_dialog(
+		"sensitiveinformation",
+		{
+			"$begin":
+			Utils.dialog_part(
+				"""
+				There is a password written on the whiteboard for everyone to see. This poses a significant security risk as it allows unauthorized individuals to easily access sensitive information
+
+
+				<?[url=$change:correct]1) I will immediately erase the password from the whiteboard to prevent unauthorized access and protect our sensitive information [/url]?>
+				<?[url=$nochange:wrong]2) I don't think it's a problem to leave the password there. We trust our team, and it's convenient for everyone to have easy access to it. [/url]?>
+				
+				"""
+			),
+			"$change":
+				Utils.dialog_part(
+				"""
+That's the correct decision. By removing the password from the whiteboard, you are taking a proactive step to enhance security and protect our data.
+<?[url=$end]EXIT[/url]?>
+				"""
+				),
+			"$nochange":
+				Utils.dialog_part(
+				"""
+That is a horrible choice. Leaving password visible compromises the security of our systems and sensitive information.
+
+It's important to prioritize data protection by removing the password from the whiteboard and implementing more secure methods for sharing passwords.
+<?[url=$end]EXIT[/url]?>
+				"""
+				),
+		},
+		"training"
+	)
+	
+func _on_area_2d_area_entered(area):
+	print("interactable!")
+	if area.is_in_group("Player"):
+		interact()
