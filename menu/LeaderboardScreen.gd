6f04043ec9263830a6739cc1f86ef6b5072674a5
Sean T <teo259@gmail.com>
2023-07-04 01:09:09 +0800

    Created the leaderboard for Admin view
    - Updated the all player retrieval method for API

---
+++ menu/LeaderboardScreen.gd
@@ -0,0 +1,48 @@
+extends Control
+var LeaderboardRecord = preload("res://menu/LeaderRecord.tscn")
+const api_svr = "http://127.0.0.1:5000"
+var result = []
+@onready var record_container = $PanelContainer/VBoxContainer/Panel/ScrollContainer/VBoxContainer
+
+func _ready() -> void:
+	clear_records()
+	get_records()
+
+func clear_records() -> void:
+	for child in record_container.get_children():
+		record_container.remove_child(child)
+		child.queue_free()
+
+func get_records():
+	# TODO Get records from API
+	print("REQUESTING FOR RESULTS....")
+	var url = api_svr + "/get_Leader_Admin"
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	var headers = ["Content-Type: application/json"]
+	await $HTTPRequest.request(url,headers, HTTPClient.METHOD_POST)
+	
+	
+func populate_data():	
+	clear_records()
+	print("fk")
+	print(result)
+	#for record in result:
+	#	print(record)
+	#	var record_node = LeaderboardRecord.instance()
+	#	record_container.add_child(record_node)
+	#	record_node.setup(record.username, record.score)
+
+func _on_request_completed(result, response_code, headers, body):
+	print("Recieving data from server")
+	var recordsString = body.get_string_from_utf8()
+	result = JSON.parse_string(recordsString)
+	print(result)
+	clear_records()
+	var record_node = LeaderboardRecord.instantiate()
+	record_container.add_child(record_node)
+	record_node.setup("Name:", "Email:", "Company:", "Score:", "Completion:")
+	for record in result:
+		print(record)
+		record_node = LeaderboardRecord.instantiate()
+		record_container.add_child(record_node)
+		record_node.setup(record[0],record[1],record[2],str(record[3]),str(record[4]))
