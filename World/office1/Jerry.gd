1b7a5696e2e0493177a02380ff174a7058d2a86f
cyrus <cyrusthedark@gmail.com>
2023-07-10 14:50:58 +0800

    Update laptop9 qn

--- World/office1/Jerry.gd
+++ World/office1/Jerry.gd
@@ -17,6 +17,11 @@ func _on_laptop_9__question_lookaround():
 	if interactable != null and interactable is Node:
 		interactable.toggle_visibility(true)
 
+func _on_laptop_9__question_leave():
+	state = 3
+	if interactable != null and interactable is Node:
+		interactable.toggle_visibility(true)
+
 func interact() -> void:
 	print("interaction started")
 	if state == 0:
@@ -30,7 +35,7 @@ func interact() -> void:
 					
 					I'm really thirsty.
 					
-					<?[url=$end][right]I see.[/right][/url]
+					[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]I see.[/url]?>[/right]
 					"""
 					)
 			},
@@ -47,8 +52,8 @@ func interact() -> void:
 						
 						I'm really thirsty.
 						
-						<?[url=$next][right]Hey, did you leave your PC unlocked?[/right][/url]
-						<?[url=$end][right]I see.[/right][/url]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next]Hey, did you leave your PC unlocked?[/url][/right]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]I see.[/url][/right]
 						"""
 					),
 				"$next":
@@ -58,7 +63,7 @@ func interact() -> void:
 						
 						My PC? Yea, I just left it when I came to buy a drink.
 						
-						<?[url=$next2][right]That's not a good practice. What if someone malicious came and used your PC?[/right][/url]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next2]That's not a good idea. What if someone malicious came and used your PC?[/url][/right]
 						"""
 					),
 				"$next2":
@@ -68,7 +73,7 @@ func interact() -> void:
 						
 						Oh... I didn't consider that...
 						
-						<?[url=$next3:correct][right]I've helped you lock your PC this time. Remember to do so in the future, or you might get into trouble for breaching office policies.[/right][/url]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next3:correct]I've helped you to lock your PC this time. Remember to do so in the future, or you might get into trouble for breaching office policies.[/url][/right]
 						"""
 					),
 				"$next3":
@@ -78,7 +83,7 @@ func interact() -> void:
 						
 						Thanks for telling me - I'll be more careful in the future.
 						
-						<?[url=$end][right]No problem.[/right][/url]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]No problem.[/url][/right]
 						"""
 					)
 			},
@@ -99,15 +104,69 @@ func interact() -> void:
 						
 						Thanks for telling me - I'll be more careful in the future.
 						
-						<?[url=$end][right]No problem.[/right][/url]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]No problem.[/url][/right]
 						"""
 					)
 			},
 			"policy"
 		)
+	elif state == 3:
+		hud.show_dialog(
+			"unattendedlaptop",
+			{
+				"$begin":
+					Utils.dialog_part(
+						"""
+						[b]Jerry[/b]
+						
+						I'm really thirsty.
+						
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next]Hey, did you leave your PC unlocked?[/url][/right]
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]I see.[/url][/right]
+						"""
+					),
+				"$next":
+					Utils.dialog_part(
+						"""
+						[b]Jerry[/b]
+						
+						My PC? Yea, I just left it when I came to buy a drink.
+						
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next2]You should go check your PC. I think someone changed your wallpaper.[/url][/right]
+						"""
+					),
+				"$next2":
+					Utils.dialog_part(
+						"""
+						[b]Jerry[/b]
+						
+						Changed my wallpaper? Why would anyone do that?
+						
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$next3]It could have been a prank? But if someone was able to change your wallpaper, there's no telling what other malicious actions they could have taken...[/url][/right]
+						"""
+					),
+				"$next3":
+					Utils.dialog_part(
+						"""
+						[b]Jerry[/b]
+						
+						Oh... I didn't consider that. Thanks for telling me. I'll go lock my PC now and check if they did anything else.
+						
+						[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]Keep that in mind.[/url][/right]
+						"""
+					)
+			},
+			"policy"
+		)
+		state = 2
+		emit_signal("jerrydone")
+		if interactable != null and interactable is Node:
+			interactable.toggle_visibility(false)
 		
 func _physics_process(delta: float) -> void:
 	if Score.get_has_correct("unattendedlaptop","policy") == true:
 		state = 2
 		if interactable != null and interactable is Node:
 			interactable.toggle_visibility(false)
+
+
