da376ac52f4f97977a44f7445ea22d9108bb6747
leechernhao <2100723@sit.singaporetech.edu.sg>
2023-07-13 15:14:06 +0800

    foodcourt-moveablecharacter(inprogress)

--- World/foodcourt1/helpdesk.gd
+++ World/foodcourt1/helpdesk.gd
@@ -1,8 +1,10 @@
 extends Sprite2D
 
 @onready var hud := $"/root/FoodCourt/HUD"
+@onready var interactable := $"/root/FoodCourt/QuestionmarkFolder/helpdeskgirl"
 
 func _ready():
+	interactable.exclamation_mark()
 	pass
 	
 func interact() -> void:
@@ -38,4 +40,12 @@ func interact() -> void:
 		"training"
 	)
 
+func _on_area_2d_area_entered(area):
+	print("interactable!")
+	if area.is_in_group("Player"):
+		interact()
+
+func _physics_process(delta: float) -> void:
+	if interactable!= null and interactable is Node:
+		interactable.visible  = !Score.get_has_correct("helpdesk","socialengineering")
 
---
+++ World/foodcourt1/movingcharacter/greyshirtguy/greyshirtguy.gd
@@ -0,0 +1,48 @@
+extends AnimatedSprite2D
+
+# NPC movement variables
+var speed = 20  # Adjust this value to control the NPC's speed
+var target_positions = [Vector2(-120, 102),Vector2(-98,76),Vector2(-169,75),Vector2(-170,103)]  # Replace with your desired target positions
+var current_target_index = 0
+
+#@onready var hud := $"/root/FoodCourt/HUD"
+
+# AnimatedSprite node reference
+#@onready var animatedSprite = $FoodCourt/Movablecharacter/AnimatedSprite2D
+@onready var animatedSprite := $"/root/FoodCourt/Movablecharacter/greyshirtguy"
+
+
+
+func _process(delta):
+	# NPC movement logic
+	var velocity = Vector2.ZERO
+
+	# Get the current target position
+	var target_position = target_positions[current_target_index]
+
+	# Determine the direction the NPC should move in (e.g., towards the target position)
+	var direction = (target_position - position).normalized()
+	velocity = direction * speed
+
+	# Move the NPC
+	position += velocity * delta
+
+	# Check if the NPC has reached the target position
+	if position.distance_to(target_position) < speed * delta:
+		# Move to the next target position
+		current_target_index += 1
+		if current_target_index >= target_positions.size():
+			current_target_index = 0
+
+			# Check if the NPC has reached the last target position to close the loop
+			if current_target_index == target_positions.size() - 1:
+				if position.distance_to(target_positions[0]) < speed * delta:
+					current_target_index = 0
+
+	# Update the animation based on the NPC's movement
+	if velocity.x > 0:
+		animatedSprite.play("walkright")
+	elif velocity.x < 0:
+		animatedSprite.play("walkleft")
+	else:
+		animatedSprite.play("idle")
