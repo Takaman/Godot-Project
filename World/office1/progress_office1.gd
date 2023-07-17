861df6b43a8ccf5412065d45e89beb883f88e7b3
Yap Jia Hao <y_jiahao@yahoo.com>
2023-07-17 17:13:04 +0800

    progress office,foodcourt

--- World/office1/progress_office1.gd
+++ World/office1/progress_office1.gd
@@ -6,37 +6,59 @@ func interact() -> void:
 	print("interaction started")
 	var dialogue = """
 
-		[center] 1) Thumbdrive <![INCOMPLETE]!> [/center]
+		[center] 1) Thumbdrive <#[INCOMPLETE]#> [/center]
 		
-		[center] 2) Weak Password <![INCOMPLETE]!> [/center]
+		[center] 2) Weak Password <#[INCOMPLETE]#> [/center]
 		
-		[center] 3) System Update <![INCOMPLETE]!> [/center]
+		[center] 3) System Update <#[INCOMPLETE]#> [/center]
 		
-		[center] 4) Messy Desk <![INCOMPLETE]!> [/center]
+		[center] 4) Messy Desk <#[INCOMPLETE]#> [/center]
 		
-		[center] 5) Adware <![INCOMPLETE]!> [/center]
+		[center] 5) Adware <#[INCOMPLETE]#> [/center]
 		
-		[center] 6) Ransomware <![INCOMPLETE]!> [/center]
+		[center] 6) Ransomware <#[INCOMPLETE]#> [/center]
 		
-		[center] 7) Insider Threat <![INCOMPLETE]!> [/center]
+		[center] 7) Insider Threat <#[INCOMPLETE]#> [/center]
 		
 		[right][img=12x12]res://World/HUD/Pointer.png[/img]<?[url=$end]EXIT[/url]?>[/right]
 	"""
 	
-	if Score.get_has_correct("thumbdrive1", "socialengineering"):
-		dialogue = dialogue.replace("Thumbdrive <![INCOMPLETE]!>", "Thumbdrive <~[COMPLETED]~>")
-	if Score.get_has_correct("passwordpolicy", "policy"):
-		dialogue = dialogue.replace("Weak Password <![INCOMPLETE]!>", "Weak Password <~[COMPLETED]~>")
-	if Score.get_has_correct("laptop5", "policy"):
-		dialogue = dialogue.replace("System Update <![INCOMPLETE]!>", "System Update <~[COMPLETED]~>")
-	if Score.get_has_correct("messyarea1", "policy"):
-		dialogue = dialogue.replace("Messy Desk <![INCOMPLETE]!>", "Messy Desk <~[COMPLETED]~>")
-	if Score.get_has_correct("laptop16", "malware"):
-		dialogue = dialogue.replace("Adware <![INCOMPLETE]!>", "Adware <~[COMPLETED]~>")
-	if Score.get_has_correct("ransomware", "malware"):
-		dialogue = dialogue.replace("Ransomware <![INCOMPLETE]!>", "Ransomware <~[COMPLETED]~>")
-	if Score.get_has_correct("insiderthreat1", "policy"):
-		dialogue = dialogue.replace("Insider Threat <![INCOMPLETE]!>", "Insider Threat <~[COMPLETED]~>")
+	if Score.get_result("thumbdrive1", "socialengineering") == 1:
+		dialogue = dialogue.replace("Thumbdrive <#[INCOMPLETE]#>", "Thumbdrive <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Thumbdrive <![WRONG]!>", "Thumbdrive <~[COMPLETED]~>")
+	if Score.get_result("passwordpolicy", "policy") == 1:
+		dialogue = dialogue.replace("Weak Password <#[INCOMPLETE]#>", "Weak Password <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Weak Password <![WRONG]!>", "Weak Password <~[COMPLETED]~>")
+	if Score.get_result("laptop5", "policy") == 1:
+		dialogue = dialogue.replace("System Update <#[INCOMPLETE]#>", "System Update <~[COMPLETED]~>")
+		dialogue = dialogue.replace("System Update <![WRONG]!>", "System Update <~[COMPLETED]~>")
+	if Score.get_result("messyarea1", "policy") == 1:
+		dialogue = dialogue.replace("Messy Desk <#[INCOMPLETE]#>", "Messy Desk <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Messy Desk <![WRONG]!>", "Messy Desk <~[COMPLETED]~>")
+	if Score.get_result("laptop16", "malware") == 1:
+		dialogue = dialogue.replace("Adware <#[INCOMPLETE]#>", "Adware <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Adware <![WRONG]!>", "Adware <~[COMPLETED]~>")
+	if Score.get_result("ransomware", "malware") == 1:
+		dialogue = dialogue.replace("Ransomware <#[INCOMPLETE]#>", "Ransomware <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Ransomware <![WRONG]!>", "Ransomware <~[COMPLETED]~>")
+	if Score.get_result("insiderthreat1", "policy") == 1:
+		dialogue = dialogue.replace("Insider Threat <#[INCOMPLETE]#>", "Insider Threat <~[COMPLETED]~>")
+		dialogue = dialogue.replace("Insider Threat <![WRONG]!>", "Insider Threat <~[COMPLETED]~>")
+		
+	if Score.get_result("thumbdrive1", "socialengineering") == 2:
+		dialogue = dialogue.replace("Thumbdrive <#[INCOMPLETE]#>", "Thumbdrive <![WRONG]!>")
+	if Score.get_result("passwordpolicy", "policy") == 2:
+		dialogue = dialogue.replace("Weak Password <#[INCOMPLETE]#>", "Weak Password <![WRONG]!>")
+	if Score.get_result("laptop5", "policy") == 2:
+		dialogue = dialogue.replace("System Update <#[INCOMPLETE]#>", "System Update <![WRONG]!>")
+	if Score.get_result("messyarea1", "policy") == 2:
+		dialogue = dialogue.replace("Messy Desk <#[INCOMPLETE]#>", "Messy Desk <![WRONG]!>")
+	if Score.get_result("laptop16", "malware") == 2:
+		dialogue = dialogue.replace("Adware <#[INCOMPLETE]#>", "Adware <![WRONG]!>")
+	if Score.get_result("ransomware", "malware") == 2:
+		dialogue = dialogue.replace("Ransomware <#[INCOMPLETE]#>", "Ransomware <![WRONG]!>")
+	if Score.get_result("insiderthreat1", "policy") == 2:
+		dialogue = dialogue.replace("Insider Threat <#[INCOMPLETE]#>", "Insider Threat <![WRONG]!>")
 
 	hud.show_dialog(
 		"hints",
