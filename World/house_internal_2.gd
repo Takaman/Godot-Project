8926d66f0e6e4759c070e65dc2e24c3dcbb0f048
cyrus <cyrusthedark@gmail.com>
2023-07-21 13:36:37 +0800

    added login checker

--- World/house_internal_2.gd
+++ World/house_internal_2.gd
@@ -1,7 +1,13 @@
 extends TileMap
 
 @onready var hud := $HUD
+@onready var sessionVar = get_node("/root/Seshvar")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hud._close_panel()
 	hud.close_player_input()
+	if sessionVar._session:
+		var username = sessionVar._session.get("username")
+	else:
+		SceneTransition.change_scene("res://../menu/Login.tscn")
